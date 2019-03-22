cbc2984a91b2d4d39672e2f6ac8729496ad8af60
namx-holi <chucktesta@live.com.au>
2019-03-22 13:01:45 +1100

    Basic app

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import flask
+#!venv/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+	return "ok"
+
+
+if __name__ == "__main__":
+	app.run(debug=True)

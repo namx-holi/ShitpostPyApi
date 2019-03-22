a72bd4307ac64da902834677297d3096f6f9e490
namx-holi <chucktesta@live.com.au>
2019-03-22 13:33:34 +1100

    Endpoint to get sentences

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 #!venv/bin/python
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from generator import SentenceGenerator
+
 
 app = Flask(__name__)
-
+s = SentenceGenerator("Words")
 
 @app.route("/")
 def index():
-	return "ok"
+	return "Hello, World"
+
+
+@app.route("/shitpost", methods=["GET"])
+def get_shitpost():
+	count_str = request.args.get("count", "1")
+
+	try:
+		count = int(count_str)
+	except ValueError:
+		return jsonify({"error": "Invalid count. Must be int."})
+
+	sentences = s.generate_sentences(count)
+	return jsonify(sentences)
+
+
+@app.route("/shitpost?n=<int:count>", methods=["GET"])
+def get_shitposts(count):
+	sentences = s.generate_sentences(count)
+	return jsonify({"sentences": sentences})
 
 
 if __name__ == "__main__":
